430225f1d7be6766e733435756c30537f25acf93
Zac Miller <zac@informatical.info>
2022-08-22 23:41:35 -0400

    Add setting to configure JSON encoder for context.
    ``django-pghistory`` now uses Django's default JSON encoder
    to serialize contexts, which supports datetimes, UUIDs,
    and other fields.
    
    You can override the JSON encoder by setting
    ``PGHISTORY_JSON_ENCODER`` to the path of the class.
    
    Type: feature

--- pghistory/tracking.py
+++ pghistory/tracking.py
@@ -4,7 +4,10 @@ import json
 import threading
 import uuid
 
+from django.conf import settings
 from django.db import connection
+from django.utils.module_loading import import_string
+
 
 _tracker = threading.local()
 
@@ -18,6 +21,14 @@ def _is_concurrent_statement(sql):
     """
     sql = sql.strip().lower() if sql else ""
     return sql.startswith("create") and "concurrently" in sql
+
+
+def _json_encoder():
+    """Retrieve the JSON encoder used for encoding context"""
+    class_path = getattr(
+        settings, "PGHISTORY_JSON_ENCODER", "django.core.serializers.json.DjangoJSONEncoder"
+    )
+    return import_string(class_path)
 
 
 def _inject_history_context(execute, sql, params, many, context):
@@ -34,9 +45,10 @@ def _inject_history_context(execute, sql, params, many, context):
     # Concurrent index creation is also incompatible with local variable
     # setting. Ignore these cases for now.
     if not cursor.name and not _is_concurrent_statement(sql):
+        encoder = _json_encoder()
         # Metadata is stored as a serialized JSON string with escaped
         # single quotes
-        metadata_str = json.dumps(_tracker.value.metadata).replace("'", "''")
+        metadata_str = json.dumps(_tracker.value.metadata, cls=encoder).replace("'", "''")
 
         sql = (
             f"SET LOCAL pghistory.context_id='{_tracker.value.id}';"
