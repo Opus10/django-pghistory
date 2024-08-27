21e7391e367ae58f6c604c6c5cd2692e8f3d618b
Alex Lokhman <alex.lokhman@gmail.com>
2024-08-27 12:28:48 +0100

    Add "created_at_function" config (#137)
    Allow changing the default PostgreSQL function used to populate "pgh_created_at" field.
    
    Type: feature

--- pghistory/trigger.py
+++ pghistory/trigger.py
@@ -3,7 +3,7 @@ import re
 import pgtrigger
 from django.db import models
 
-from pghistory import utils
+from pghistory import config, utils
 
 
 def _get_pgh_obj_pk_col(history_model):
@@ -76,7 +76,7 @@ class Event(pgtrigger.Trigger):
             and f.name in tracked_model_fields
             and f.concrete
         }
-        fields["pgh_created_at"] = "NOW()"
+        fields["pgh_created_at"] = config.created_at_function()
         fields["pgh_label"] = f"'{self.label}'"
 
         if hasattr(self.event_model, "pgh_obj"):
